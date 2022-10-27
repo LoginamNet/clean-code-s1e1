faa672e7a6fad4fa500efee57bcad71a570605b6
LoginamNet <loginamnet@gmail.com>
2022-10-27 19:33:58 +0300

    feat: add alt to images according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="button__image";
+    deleteButtonImg.alt="Close button";
     deleteButton.appendChild(deleteButtonImg);
 
 
