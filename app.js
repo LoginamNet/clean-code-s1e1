27d17bad31fe3070cb5cbd4814297f78f9e4f0ea
LoginamNet <loginamnet@gmail.com>
2022-10-28 09:30:22 +0300

    refactor: refactor IDs to lowercase according to rule 1.2 in html-and-css.md

--- app.js
+++ app.js
@@ -10,9 +10,10 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("todo-list");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed-list");//completed-tasks
 
+console.log(incompleteTaskHolder);
 
 //New task list item
 var createNewTaskElement=function(taskString){
